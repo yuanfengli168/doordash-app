a3fb58ab30f7ae05c118c024dbbdf9432d95d80c
yuanfengli168 <49966978+yuanfengli168@users.noreply.github.com>
2022-10-05 21:10:53 -0700

    Added Login Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { Layout } from "antd";
 import './App.css';
+import LoginForm from "./components/LoginForm";
+
+const { Header, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{ height: "100vh"}}>
+      <Header>Header</Header>
+      <Content style={{
+        padding: "50px",
+        maxHeight: "calc(100% - 64px)",
+        overflowY: "auto",
+      }}>
+        <LoginForm />
+      </Content>
+    </Layout>
   );
 }
 
